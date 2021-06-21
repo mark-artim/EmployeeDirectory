257740007ed458eb63f2eb26c7fd5c4d5480a146
Mark Artim <mark.artim@gmail.com>
2021-06-21 11:12:35 -0500

    Completed README file with screenshot and modified search results to bring back 200 employees from 50

--- empdir/src/utils/API.js
+++ empdir/src/utils/API.js
@@ -5,12 +5,12 @@ export default {
     getEmployees: function() {
         return new Promise((resolve, reject) => {
             axios
-            .get("https://randomuser.me/api/?results=50&inc=name,picture,email,cell,location")
+            .get("https://randomuser.me/api/?results=200&inc=name,picture,email,cell,location")
             .then(res => {
                 const employees = res.data;
                 const results = res.data;
                 // const results = employees.map(employee => {
-                    console.log('API!! employees>>>>>',employees)
+                    // console.log('API!! employees>>>>>',employees)
                 //     return {
                 //         firstname: employee.name.first,
                 //         lastname: employee.name.last,
@@ -23,35 +23,5 @@ export default {
         });
     }
   };
-
-// const Employee = () => {
-//     // const [hasError, setErrors] = useState(false);
-//     // const [employee, setEmployee] = useState({});
-
-//     async function fetchData() {
-//         const res = await fetch ("https://randomuser.me/api/");
-//             res
-//             .json()
-//             // .then(res => setEmployee(res))
-//             // .catch(err);
-//     }
-
-    // useEffect(() => {
-    //     fetchData();
-    // },[]);
-
-    // return (
-    //     <div>
-    //     <span>{JSON.stringify(employee)}</span>
-    //     <hr />
-    //     <span>Has error: {JSON.stringify(hasError)}</span>
-    //     </div>
-    // );
-    // };
-
-// export default Employee;
-
-
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 
